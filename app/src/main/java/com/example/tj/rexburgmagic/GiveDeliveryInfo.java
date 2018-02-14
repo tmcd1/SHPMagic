f0d78e0593e27368e2a6d61dc809c77142ad1da8
tmcd1 <t.jared.mcd@gmail.com>
2018-02-14 08:36:24 -0700

    Added basic delivery activity

---
+++ app/src/main/java/com/example/tj/rexburgmagic/GiveDeliveryInfo.java
@@ -0,0 +1,17 @@
+package com.example.tj.rexburgmagic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GiveDeliveryInfo extends AppCompatActivity {
+
+    public void getHowManySupplies(View view) {
+        // Respond to button press in Delivery Info Activity
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_delivery_info);
+    }
+}
